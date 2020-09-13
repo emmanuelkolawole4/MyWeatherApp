d2438b4b8ccc2abeb702026a2b10c848b784acbd
emmanuelkolawole4 <emmanuelkolawole4@gmail.com>
2020-09-13 19:22:36 +0100

    User Interface almost done

---
+++ MyWeatherApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by FOLAHANMI KOLAWOLE on 13/09/2020.
+//  Copyright © 2020 FOLAHANMI KOLAWOLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+
+}
+
